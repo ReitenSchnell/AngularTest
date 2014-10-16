96e883b77f1c34382b4aabcd20c22316ed095838
Alina.Gryaznova <Alina.Gryaznova@sigmaukraine.com>
2014-10-16 16:06:00 +0300

    adds posts controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,7 @@
  *
  * Main module of the application.
  */
-angular
+var app = angular
   .module('angularTestApp', [
     'ngAnimate',
     'ngCookies',
@@ -16,18 +16,11 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch'
-  ])
-  .config(function ($routeProvider) {
+  ]);
+  app.config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
+        templateUrl: 'views/posts.html',
+        controller: 'PostCtrl'
       });
   });
