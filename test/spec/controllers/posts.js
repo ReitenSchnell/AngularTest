9b22637212f3e04d75a045a7aef8f6f98a00d670
Alina.Gryaznova <Alina.Gryaznova@sigmaukraine.com>
2014-10-17 13:49:59 +0300

    adds tests and post logic

---
+++ test/spec/controllers/posts.js
@@ -0,0 +1,44 @@
+describe('Controller: PostsController', function(){
+
+    beforeEach(module('angularTestApp'));
+    var PostCtrl, scope;
+
+    beforeEach(inject(function($controller, $rootScope){
+        scope = $rootScope.$new();
+        PostCtrl = $controller('PostCtrl', {
+            $scope:scope
+        });
+    }));
+
+    it('should create posts array', function(){
+        expect(scope.posts.length).toBe(0)
+    });
+
+    it('should create initial post', function(){
+        expect(scope.post.title).toBe('');
+        expect(scope.post.url).toBe('http://')
+    });
+
+    it('should add post to array on submit', function(){
+        var title = 'some title';
+        scope.post.title = title;
+        var url = 'ya.ru';
+        scope.post.url = url;
+
+        scope.submitPost();
+
+        expect(scope.posts[0].title).toBe(title);
+        expect(scope.posts[0].url).toBe(url);
+        expect(scope.posts.length).toBe(1);
+    });
+
+    it('should clear post on submit', function(){
+        scope.post.title = 'some title';
+        scope.post.url = 'ya.ru';
+
+        scope.submitPost();
+
+        expect(scope.post.title).toBe('');
+        expect(scope.post.url).toBe('http://');
+    });
+});
