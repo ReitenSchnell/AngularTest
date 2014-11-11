0300e2b5a10545330e6510594e95327d22ed3364
Alina.Gryaznova <Alina.Gryaznova@sigmaukraine.com>
2014-11-11 18:25:43 +0200

    adds tests for auth controller

--- test/spec/controllers/authController.spec.js
+++ test/spec/controllers/authController.spec.js
@@ -2,7 +2,10 @@
 describe('Controller:AuthCtrl', function(){
 
     var authCtrl, scope, deferredSuccess, location;
-    var authServiceMock = {};
+    var authServiceMock = {
+        register : function(){},
+        login : function(){}
+    };
     var testUser = {};
 
     beforeEach(module('angularTestApp'));
@@ -11,6 +14,8 @@ describe('Controller:AuthCtrl', function(){
         deferredSuccess = $q.defer();
         location = $location;
         spyOn(location, 'path').andCallThrough();
+        spyOn(authServiceMock, 'register').andCallThrough().andReturn(deferredSuccess.promise);
+        spyOn(authServiceMock, 'login').andCallThrough().andReturn(deferredSuccess.promise);
         authCtrl = $controller('AuthCtrl', {
             $scope:scope,
             authService:authServiceMock,
@@ -19,6 +24,28 @@ describe('Controller:AuthCtrl', function(){
     }));
 
     it('should redirect to home page when user is logged in', function(){
+        expect(location.path).toHaveBeenCalledWith('/');
+    });
+
+    it('should call service register when registering user', function(){
+        scope.user = testUser;
+        scope.register();
+        expect(authServiceMock.register).toHaveBeenCalledWith(testUser);
+    });
+
+    it('should call service login when registering user', function(){
+        scope.user = testUser;
+        deferredSuccess.resolve();
+        scope.register();
+        scope.$apply();
+        expect(authServiceMock.login).toHaveBeenCalledWith(testUser);
+    });
+
+    it('should redirect to home page when registered user logged in', function(){
+        scope.user = testUser;
+        deferredSuccess.resolve();
+        scope.register();
+        scope.$apply();
         expect(location.path).toHaveBeenCalledWith('/');
     })
 });
